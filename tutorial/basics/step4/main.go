7d4a70dd7fbd6f4aeab458bff3bddda1437c2448
Gautam Dey <gautam.dey77@gmail.com>
2022-10-06 11:17:15 -0500

    Minor change for step4 to make things a bit easier to read.

--- tutorial/basics/step4/main.go
+++ tutorial/basics/step4/main.go
@@ -9,22 +9,32 @@ import (
 	"tinygo.org/x/drivers/ws2812"
 )
 
+var (
+	Red   = color.RGBA{0, 255, 0, 255}
+	Green = color.RGBA{0, 255, 0, 255}
+)
+
 func main() {
+
+	// get and configure neopixels
 	neo := machine.NEOPIXELS
 	neo.Configure(machine.PinConfig{Mode: machine.PinOutput})
 
 	leds := ws2812.New(neo)
 	ledColors := make([]color.RGBA, 5)
 
+	// Initialize the Analog to digital subsystem
 	machine.InitADC()
 	light := machine.ADC{machine.LIGHTSENSOR}
 	light.Configure(machine.ADCConfig{})
 
 	for {
+
+		// clamp the light value from the sensor
 		value := uint8(light.Get() / 256)
-		c := color.RGBA{0, 255, 0, 255}
+		c := Green
 		if value < 25 {
-			c = color.RGBA{255, 0, 0, 255}
+			c = Red
 		}
 		for i := 0; i < 5; i++ {
 			ledColors[i] = c
