f84c54b8f9bed44b61702a850c859e6e4ca7f2d3
Gautam Dey <gautam.dey77@gmail.com>
2022-10-05 22:54:09 -0500

    Step 0 documentation and images.

--- tutorial/basics/step0/main.go
+++ tutorial/basics/step0/main.go
@@ -6,13 +6,24 @@ import (
 )
 
 func main() {
+
+	// get the pin that represents that LED on the back of the device D13
 	led := machine.LED
+
+	// configuring the LED pin mode for output
 	led.Configure(machine.PinConfig{Mode: machine.PinOutput})
+
 	for {
+		// turn off the LED
 		led.Low()
+
+		// wait 500 ms
 		time.Sleep(time.Millisecond * 500)
 
+		// turn on the LED
 		led.High()
+
+		// wait 500 ms
 		time.Sleep(time.Millisecond * 500)
 	}
 }
